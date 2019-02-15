fb6287d782f9b246cb3e6e9ae1205d9c733c3caf
Murillo Camargo <murillo@kiplingmedia.com>
2019-02-15 11:13:47 -0800

    Form integration

--- wp-content/themes/ravi-bhindi/page-find-an-agent.php
+++ wp-content/themes/ravi-bhindi/page-find-an-agent.php
@@ -16,80 +16,95 @@
             </div>
 
             <div class="col">
-                <form action="">
-                    <div class="form-group">
-                        <label for="firstName" class="sr-only">First Name</label>
-                        <input type="text" class="form-control" id="firstName" placeholder="First Name">
-                    </div>
 
-                    <div class="form-group">
-                        <label for="lastName" class="sr-only">Last Name</label>
-                        <input type="text" class="form-control" id="lastName" placeholder="Last Name">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="lastName" class="sr-only">Your E-mail</label>
-                        <input type="email" class="form-control" id="email" placeholder="E-mail address">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="phoneNumber" class="sr-only">Phone Number</label>
-                        <input type="text" class="form-control" id="phoneNumber" placeholder="Phone Number">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="location">Where?</label>
-                        <select class="form-control" id="location">
-                            <option value="#Vancouver">Vancouver</option>
-                            <option value="#Burnaby">Burnaby</option>
-                            <option value="#Richmond">Richmond</option>
-                            <option value="#NewWest">New West</option>
-                            <option value="#Surrey">Surrey</option>
-                            <option value="#WhiteRock">White Rock</option>
-                            <option value="#Abbotsford">Abbotsford</option>
-                            <option value="#MapleRidge">Maple Ridge</option>
-                            <option value="#PittMeadows">Pitt Meadows</option>
-                            <option value="#NorthVancouver">North Vancouver</option>
-                            <option value="#PortCoquitlam">Port Coquitlam</option>
-                            <option value="#Coquitlam">Coquitlam</option>
-                            <option value="#PortMoody">Port Moody</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <p>Are you a Realtor?</p>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="realtor" id="realtor1" value="yes">
-                            <label class="form-check-label" for="realtor1">Yes</label>
+                <div role="form" class="wpcf7" id="wpcf7-f221-o1" lang="en-US" dir="ltr">
+                    <div class="screen-reader-response"></div>
+                    <form action="/find-an-agent/#wpcf7-f221-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+                        <div style="display: none;">
+                            <input type="hidden" name="_wpcf7" value="221">
+                            <input type="hidden" name="_wpcf7_version" value="5.1.1">
+                            <input type="hidden" name="_wpcf7_locale" value="en_US">
+                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f221-o1">
+                            <input type="hidden" name="_wpcf7_container_post" value="0">
+                            <input type="hidden" name="g-recaptcha-response" value="">
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="realtor" id="realtor2" value="no">
-                            <label class="form-check-label" for="realtor2">No</label>
+                        <div class="form-group">
+                            <label for="firstName" class="sr-only">First Name</label>
+                            <span class="wpcf7-form-control-wrap firstName"><input type="text" name="firstName" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="First Name"></span>
                         </div>
-                    </div>
 
-                    <div class="form-group">
-                        <p>Are you working with a Realtor?</p>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="realtor" id="realtor1" value="yes">
-                            <label class="form-check-label" for="realtor1">Yes</label>
+                        <div class="form-group">
+                            <label for="lastName" class="sr-only">Last Name</label>
+                            <span class="wpcf7-form-control-wrap lastName"><input type="text" name="lastName" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Last Name"></span>
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="realtor" id="realtor2" value="no">
-                            <label class="form-check-label" for="realtor2">No</label>
+
+                        <div class="form-group">
+                            <label for="yourEmail" class="sr-only">Your E-mail</label>
+                            <span class="wpcf7-form-control-wrap yourEmail"><input type="email" name="yourEmail" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-control" aria-required="true" aria-invalid="false" placeholder="E-mail address"></span>
                         </div>
-                    </div>
 
-                    <div class="form-group">
-                        <label for="message" class="sr-only">Message</label>
-                        <textarea class="form-control" id="message" rows="5" placeholder="Add your message"></textarea>
-                    </div>
+                        <div class="form-group">
+                            <label for="phoneNumber" class="sr-only">Phone Number</label>
+                            <span class="wpcf7-form-control-wrap phoneNumber"><input type="text" name="phoneNumber" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Phone Number"></span>
+                        </div>
 
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-submit">Submit</button>
-                    </div>
-                </form>
+                        <div class="form-group">
+                            <label for="location">Where?</label>
+                            <span class="wpcf7-form-control-wrap location">
+                                <select name="location" class="wpcf7-form-control wpcf7-select form-control" id="location" aria-invalid="false">
+                                    <option value="">---</option>
+                                    <option value="#Vancouver">Vancouver</option>
+                                    <option value="#Burnaby">Burnaby</option>
+                                    <option value="#Richmond">Richmond</option>
+                                    <option value="#NewWest">New West</option>
+                                    <option value="#Surrey">Surrey</option>
+                                    <option value="#WhiteRock">White Rock</option>
+                                    <option value="#Abbotsford">Abbotsford</option>
+                                    <option value="#MapleRidge">Maple Ridge</option>
+                                    <option value="#PittMeadows">Pitt Meadows</option>
+                                    <option value="#NorthVancouver">North Vancouver</option>
+                                    <option value="#PortCoquitlam">Port Coquitlam</option>
+                                    <option value="Coquitlam">Coquitlam</option>
+                                    <option value="#PortMoody">Port Moody</option>
+                                </select>
+                            </span>
+                        </div>
+
+                        <div class="form-group">
+                            <p>Are you a Realtor?</p>
+                            <div class="form-check form-check-inline">
+                                <span class="wpcf7-form-control-wrap realtor"><span class="wpcf7-form-control wpcf7-radio form-check-input" id="realtor1"><span class="wpcf7-list-item first last"><input type="radio" name="realtor" value="Yes"><span class="wpcf7-list-item-label">Yes</span></span></span></span>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <span class="wpcf7-form-control-wrap realtor"><span class="wpcf7-form-control wpcf7-radio form-check-input" id="realtor2"><span class="wpcf7-list-item first last"><input type="radio" name="realtor" value="No"><span class="wpcf7-list-item-label">No</span></span></span></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <p>Are you working with a Realtor?</p>
+                            <div class="form-check form-check-inline">
+                                <span class="wpcf7-form-control-wrap workingRealtor"><span class="wpcf7-form-control wpcf7-radio form-check-input" id="realtor1"><span class="wpcf7-list-item first last"><input type="radio" name="workingRealtor" value="Yes"><span class="wpcf7-list-item-label">Yes</span></span></span></span>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <span class="wpcf7-form-control-wrap workingRealtor"><span class="wpcf7-form-control wpcf7-radio form-check-input" id="realtor2"><span class="wpcf7-list-item first last"><input type="radio" name="workingRealtor" value="No"><span class="wpcf7-list-item-label">No</span></span></span></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="message" class="sr-only">Message</label>
+                            <span class="wpcf7-form-control-wrap message"><textarea name="message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Add your message"></textarea></span>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" value="Send" class="wpcf7-form-control wpcf7-submit btn btn-submit"><span class="ajax-loader"></span>
+                        </div>
+                        <div class="wpcf7-response-output wpcf7-display-none"></div>
+                    </form>
+                </div>
+                <!-- /FORM --->
+
             </div>
+            <!-- /.col -->
 
         </div>
         <!-- /.row -->
