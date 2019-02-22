bf4e7fb50f54f41052d64e6b7a8749c0cd3bc170
Murillo Camargo <murillo@kiplingmedia.com>
2019-02-22 12:23:19 -0800

    Page WP Loop

--- wp-content/themes/ravi-bhindi/page-find-an-agent.php
+++ wp-content/themes/ravi-bhindi/page-find-an-agent.php
@@ -12,7 +12,10 @@
             <!-- /.col-12 -->
 
             <div class="col">
-                <?php the_content(); ?>
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php the_content(); ?>
+                <?php endwhile; else : ?>
+                <?php endif; ?>
             </div>
 
             <div class="col">
