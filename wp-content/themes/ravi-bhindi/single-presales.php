01e7ef185bb6a3f75a0c616d2afecee03d1f7ca6
Murillo Camargo <murillo@kiplingmedia.com>
2019-03-22 11:29:59 -0700

    Adding CPT Presales single template

---
+++ wp-content/themes/ravi-bhindi/single-presales.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+
+        <h1><?php the_title(); ?></h1>
+
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /.container -->
+
+<?php get_footer(); ?>
