590a6d4daa83abba7ebd78cf233b9e87656e9d80
Murillo Camargo <murillo@kiplingmedia.com>
2019-03-25 10:29:53 -0700

    Presales page base HTML Markup

--- wp-content/themes/ravi-bhindi/single-presales.php
+++ wp-content/themes/ravi-bhindi/single-presales.php
@@ -3,10 +3,57 @@
 <div class="container">
     <div class="row">
 
-        <h1><?php the_title(); ?></h1>
+        <div class="col-12">
+            <ul class="breadcrumb">
+                <li><a href="javascript:history.go(-1)" title="Return to the previous page">« Go back</a></li>
+            </ul>
+        </div>
+
+        <header id="presales-header" class="col-12">
+            <h1><?php the_title(); ?></h1>
+        </header>
+        <!-- /#presales-header.col-12 -->
+
+        <div class="col-12">
+            <div class="presales-featured-img">
+                <img src="" alt="">
+            </div>
+            <!-- /.presales-featured-img -->
+        </div>
+        <!-- /.col-12 -->
+
+        <div id="presales-description" class="col-12">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cum distinctio, eos iure labore odit porro quasi suscipit ut voluptate.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias autem blanditiis doloribus, dolorum eos error, ex fugiat illo iure necessitatibus odio quam quas repudiandae ullam voluptatum. Eligendi fugit nihil voluptas!</p>
+        </div>
+        <!-- /#presales-description.col-12 -->
 
     </div>
     <!-- /.row -->
+
+    <div id="presales-images" class="row">
+        <div class="presale-img">
+            <img src="" alt="">
+        </div>
+    </div>
+    <!-- /#presales-images.row -->
+
+    <div id="presales-map" class="row">
+        <div class="col-12">
+            <div class="presales-address">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, nam.</p>
+            </div>
+            <!-- /.presales-address -->
+        </div>
+        <!-- /.col-12 -->
+
+        <div class="col-12">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83327.54691405174!2d-123.19394412549934!3d49.25771428116025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673f143a94fb3%3A0xbb9196ea9b81f38b!2sVancouver%2C+BC!5e0!3m2!1sen!2sca!4v1553534808378" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+    <!-- /#presales-map.row -->
 </div>
 <!-- /.container -->
 
