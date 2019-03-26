ada7dc6fbb9e48c818b6602cbd49861fea1a178a
Murillo Camargo <murillo@kiplingmedia.com>
2019-03-26 10:28:15 -0700

    Questionnaire page

---
+++ wp-content/themes/ravi-bhindi/page-questionnaire.php
@@ -0,0 +1,224 @@
+<?php
+/*
+Template Name: Questionnaire
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="questionnaire-content">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12">
+                <div id="featured-video" class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="<?php the_field('questionnaire_video'); ?>" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-9 mx-md-auto">
+
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Name">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Address">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="His Work Address">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Her Work Address">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Cell Phone">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Home or Work Phone">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="E-mail Address">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Do you presently own your home?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Are you required to sell your present home to purchase your new home?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Are you required to sell your present home to purchase your new home?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Have you obtained a Pre-Approved mortgage?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="How much have you been approved for?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What is your available down payment?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Why are you considering a move?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="When would you like to move?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What price range are you considering?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="How much more can you push your price range by?">
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="How many bedrooms?">
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="How many bathrooms?">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What would be your ideal travel distance to work?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What total square footage are you looking for in your next home?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Are there neighborhoods you are interested in?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What amenities would you like to have access to in your new neighborhood? (e.g. schools, shops, churches, public transit, etc)">
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-12">
+                            <p>How many people will be living in the homes?</p>
+                        </div>
+
+                        <div class="w-100"></div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Adults">
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Children">
+                            </div>
+                        </div>
+
+                        <div class="w-100"></div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="How many Parking spots?">
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Do you have Pets?">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Are you prepared to carry out repairs or renovations on your next home?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Is underground parking important to you?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Tell me about your yard space requirements?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Is the age of the house important? If so what age?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="When are you looking at completing on your purchase?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="When are the best times for you to view properties?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What are the best times for me to reach you?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Who needs to be present during showings?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What are you most concerned about your next home?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What are you most looking forward to once you move into your new home?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="If we could find the right house today, are you in a position to buy today?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Other important information that will help us in the search for your next home?">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="What expectations do you have of me, your Realtor?">
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-submit form-submit">Send</button>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</div>
+<!-- /#questionnaire-content -->
+
+<?php get_footer(); ?>
