c9fda941bbbe6fdeddb40ee1b5350d3d2aa27e44
Murillo Camargo <murillo@kiplingmedia.com>
2019-05-06 12:33:18 -0700

    Fix form

--- wp-content/themes/ravi-bhindi/page-find-an-agent.php
+++ wp-content/themes/ravi-bhindi/page-find-an-agent.php
@@ -20,37 +20,33 @@
 
             <div class="col">
 
-                <div role="form" class="wpcf7" id="wpcf7-f214-o1" lang="en-US" dir="ltr">
+                <div role="form" class="wpcf7" id="wpcf7-f7-o1" lang="en-US" dir="ltr">
                     <div class="screen-reader-response"></div>
-                    <form action="/find-and-agent/#wpcf7-f214-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+                    <form action="/find-an-agent/#wpcf7-f7-o1" method="post" class="wpcf7-form mailchimp-ext-0.4.51" novalidate="novalidate">
                         <div style="display: none;">
-                            <input type="hidden" name="_wpcf7" value="214">
+                            <input type="hidden" name="_wpcf7" value="7">
                             <input type="hidden" name="_wpcf7_version" value="5.1.1">
                             <input type="hidden" name="_wpcf7_locale" value="en_US">
-                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f214-o1">
+                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f7-o1">
                             <input type="hidden" name="_wpcf7_container_post" value="0">
                             <input type="hidden" name="g-recaptcha-response" value="">
                         </div>
                         <div class="form-group">
-                            <label for="firstName" class="sr-only">First Name</label>
+                            <label for="firstName" class="sr-only">First Name</label><br>
                             <span class="wpcf7-form-control-wrap firstName"><input type="text" name="firstName" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="First Name"></span>
                         </div>
-
                         <div class="form-group">
-                            <label for="lastName" class="sr-only">Last Name</label>
+                            <label for="lastName" class="sr-only">Last Name</label><br>
                             <span class="wpcf7-form-control-wrap lastName"><input type="text" name="lastName" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Last Name"></span>
                         </div>
-
                         <div class="form-group">
-                            <label for="yourEmail" class="sr-only">Your E-mail</label>
+                            <label for="yourEmail" class="sr-only">Your E-mail</label><br>
                             <span class="wpcf7-form-control-wrap yourEmail"><input type="email" name="yourEmail" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-control" aria-required="true" aria-invalid="false" placeholder="E-mail address"></span>
                         </div>
-
                         <div class="form-group">
-                            <label for="phoneNumber" class="sr-only">Phone Number</label>
+                            <label for="phoneNumber" class="sr-only">Phone Number</label><br>
                             <span class="wpcf7-form-control-wrap phoneNumber"><input type="text" name="phoneNumber" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Phone Number"></span>
                         </div>
-
                         <div class="form-group">
                             <label for="location">Where?</label>
                             <span class="wpcf7-form-control-wrap location">
@@ -72,7 +68,6 @@
                                 </select>
                             </span>
                         </div>
-
                         <div class="form-group">
                             <p>Are you a Realtor?</p>
                             <div class="form-check form-check-inline">
@@ -81,8 +76,7 @@
                             <div class="form-check form-check-inline">
                                 <span class="wpcf7-form-control-wrap realtor"><span class="wpcf7-form-control wpcf7-radio form-check-input" id="realtor2"><span class="wpcf7-list-item first last"><input type="radio" name="realtor" value="No"><span class="wpcf7-list-item-label">No</span></span></span></span>
                             </div>
-                        </div>
-
+                            <p></p></div>
                         <div class="form-group">
                             <p>Are you working with a Realtor?</p>
                             <div class="form-check form-check-inline">
@@ -91,18 +85,17 @@
                             <div class="form-check form-check-inline">
                                 <span class="wpcf7-form-control-wrap workingRealtor"><span class="wpcf7-form-control wpcf7-radio form-check-input" id="realtor2"><span class="wpcf7-list-item first last"><input type="radio" name="workingRealtor" value="No"><span class="wpcf7-list-item-label">No</span></span></span></span>
                             </div>
-                        </div>
-
+                            <p></p></div>
                         <div class="form-group">
-                            <label for="message" class="sr-only">Message</label>
+                            <label for="message" class="sr-only">Message</label><br>
                             <span class="wpcf7-form-control-wrap message"><textarea name="message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Add your message"></textarea></span>
                         </div>
-
                         <div class="form-group">
                             <input type="submit" value="Send" class="wpcf7-form-control wpcf7-submit btn btn-submit"><span class="ajax-loader"></span>
                         </div>
                         <div class="wpcf7-response-output wpcf7-display-none"></div>
-                    </form>
+                        <p style="display: none !important"><span class="wpcf7-form-control-wrap referer-page"><input type="hidden" name="referer-page" value="http://emailmelistings.com/" class="wpcf7-form-control wpcf7-text referer-page" aria-invalid="false"></span></p>
+                        <!-- Chimpmail extension by Renzo Johnson --></form>
                 </div>
 
             </div>
